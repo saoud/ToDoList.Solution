23b34e14df5eb71960a2acf21ccb855c7f83d9f6
Saoud Rana <saoud@saoud.dev>
2021-04-17 15:14:18 -0700

    adding in second test

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,3 +1,5 @@
+
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
 
@@ -7,7 +9,21 @@ namespace ToDoList.Tests
   public class ItemTests
   {
 
-    // Test methods will go here.
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item();
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string result = newItem.Description;
+      Assert.AreEqual(description, result);
+    }
 
   }
 }
