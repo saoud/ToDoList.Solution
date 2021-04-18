b414131a4cb8c40f66f55ed02a83ada8539f99e1
Saoud <saoud@saoud.dev>
2021-04-18 11:44:39 -0700

    all 5 tests passing, added Dispose method
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,9 +1,23 @@
+using System.Collections.Generic;
+// using System;
 namespace ToDoList.Models
 {
   public class Item
   {
-
-    // properties, methods, etc. will go here.
-
+  public string Description { get; set; }
+  private static List<Item> _instances = new List<Item> {};
+  public Item(string description)
+  {
+    Description = description;
+    _instances.Add(this); // New code.
+  }
+  public static List<Item> GetAll()
+{
+    return _instances;
+}
+public static void ClearAll()
+{
+  _instances.Clear();
+}
   }
 }
